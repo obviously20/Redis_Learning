c6353f0ca931e4ec175dda48cd47b7ebf6d81dc2
obviously20 <1317418062@qq.com>
2026-09-14 18:54:02 +0800

    发布+查看探店笔记功能代码的完善

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,17 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    /**
+     * 查询热门探店博文
+     * @param current
+     * @return
+     */
+    Result queryHotBlog(Integer current);
+
+    /**
+     * 根据id查询探店博文
+     * @param id
+     * @return
+     */
+    Result queryBlogById(Long id);
 }
